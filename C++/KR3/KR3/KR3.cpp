94d8dd576dfb95edad65d1545b89abe7f7eae661
VladoZAVR21 <vladozavr228@gmail.com>
2022-12-02 13:13:07 +0300

    KT4

---
+++ C++/KR3/KR3/KR3.cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <clocale>
+#include <algorithm>
+#include <time.h>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+void Program1() {
+	const int n = 255;
+	char s[n];
+	int slovo, count = 0;
+	cout << "Введите строку: " << endl;
+	cin.get(s, n);
+	int i = 0;
+	while (s[i] == ' ' && s[i] != '\0')
+		i++;
+	slovo = 0;
+	while (s[i] != '\0') {
+		if (s[i] != ' ' && slovo == 0)
+		{
+			slovo = 1;
+			count++;
+		}
+		else if (s[i] == ' ')
+			slovo = 0;
+		i++;
+	}
+	cout << "Количество слов в строке " << count <<endl;
+}
+
+void Program2(double Xn, double Xk, double dX, double e, int prec) {
+	int n;
+	double nextX, Y;
+	cout.precision(prec);
+
+	cout << ("_________________________") << endl;
+	cout << ("|    x  |    y  |   N   |") << endl;
+	cout << ("|_______|_______|_______|") << endl;
+
+	for (; Xn <= Xk; Xn += dX)
+	{
+		nextX = 1.0;
+		Y = 0;
+		n = 1;
+		do {
+			nextX = (1.0 / (n * pow(Xn, n)));
+			Y = Y + nextX;
+			n = n + 2;
+		} while (abs(nextX) > e);
+		cout << setw(8) << Xn << ' ' << 2 * Y << ' ' << n << endl;
+	}
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	Program1();
+	double Xn=1, Xk, dX, e, nextX, Y;
+	int n;
+	int prec = 7;
+	cout << "Введите желаемое видимое количество знаков после запятой (предпочетаемо 7): "; cin >> prec;
+	cout.precision(prec);
+	cout << "Xнач не должно быть равно 1. Отображаемая точность до " << prec << " знаков после запятой " << endl;
+	while (Xn == 1) {
+		cout << ("Введите начальное значене Xнач: "); cin >> Xn;
+	}
+
+	cout << ("Введите конечное значение Xкон: "); cin >> Xk;
+	cout << ("Введите шаг dX: "); cin >> dX;
+	cout << ("Введите точность e: "); cin >> e;
+	Program2(Xn, Xk, dX, e, prec);
+	system("PAUSE");
+	
+	return 0;
+}
--- YaProg/Pr/KT4/Pr11/Pr11.py
+++ YaProg/Pr/KT4/Pr11/Pr11.py
@@ -2,8 +2,9 @@ import traceback
 from tkinter import *
 import requests
 
-def data_site(url):
+def data_site():
     try:
+        url = 'https://api.github.com/users/' + input_text.get()
         site = requests.get(url).json()
         company = site['company']
         created_at = site['created_at']
@@ -18,23 +19,23 @@ def data_site(url):
             f.write('name: ' + str(name) + '\n')
             f.write('url: ' + str(url) + '\n')
         print('Done!')
+        '''Вывод ошибки в случае чего'''
     except Exception as e:
         print('Error')
         print(traceback.format_exc())
 
-url = 'https://api.github.com/users/'
+window = Tk()
+window.title('Заварзин Владислав')
+window.geometry('400x150+0+0')
+window.resizable(False, False)
+'''Расположение окна посреди экрана'''
+x = (window.winfo_screenwidth() - window.winfo_reqwidth()) / 2.35
+y = (window.winfo_screenheight() - window.winfo_reqheight()) / 2.35
+window.wm_geometry("+%d+%d" % (x, y))
 
-root = Tk()
-root.title('Get data')
-root.geometry('400x150+0+0')
-root.resizable(False, False)
-x = (root.winfo_screenwidth() - root.winfo_reqwidth()) / 2.35
-y = (root.winfo_screenheight() - root.winfo_reqheight()) / 2.35
-root.wm_geometry("+%d+%d" % (x, y))
-
-input_text = Entry(root, width=50)
+input_text = Entry(window, width=50)
 input_text.pack(anchor=N)
 
-btn = Button(root, text='Get value', width=10, height=5, command=lambda: [data_site(url + input_text.get())]).pack()
+btn = Button(window, text='Получить данные', width=15, height=5, command=data_site).pack()
 
-root.mainloop()
+window.mainloop()
